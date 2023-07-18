3fb4d1f22b2e2df6d497149c890dd7abc7b137ca
GonzDv <xliqn0@gmail.com>
2023-07-18 00:15:56 -0600

    Upgrade pages

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-function deleteClass() {
-  const postres = document.getElementsByClassName("postres");
-  const tostadas = document.getElementsByClassName("tostadas");
-  var lbp = window.matchMedia("(max-width:1400px)");
-  var mbp = window.matchMedia("(max-width:1200px)");
+// function deleteClass() {
+//   const postres = document.getElementsByClassName("postres");
+//   const tostadas = document.getElementsByClassName("tostadas");
+//   var lbp = window.matchMedia("(max-width:1400px)");
+//   var mbp = window.matchMedia("(min-width:768px)");
 
-  if (lbp.matches) {
-    for (let i = 0; i < postres.length; i++) {
-      postres[i].classList.remove("shu2");
-    }
-  } else {
-    for (let i = 0; i < postres.length; i++) {
-      postres[i].classList.add("shu2");
-    }
-  }
-  if (lbp.matches) {
-    for (let i = 0; i < tostadas.length; i++) {
-      tostadas[i].classList.remove("shu2");
-    }
-  } else {
-    for (let i = 0; i < tostadas.length; i++) {
-      tostadas[i].classList.add("shu2");
-    }
-  }
-}
+//   if (lbp.matches) {
+//     for (let i = 0; i < postres.length; i++) {
+//       postres[i].classList.remove("size2");
+//     }
+//   } else {
+//     for (let i = 0; i < postres.length; i++) {
+//       postres[i].classList.add("size2");
+//     }
+//   }
+//   if (lbp.matches) {
+//     for (let i = 0; i < tostadas.length; i++) {
+//       tostadas[i].classList.add("size2");
+//     }
+//   } else {
+//     for (let i = 0; i < tostadas.length; i++) {
+//       tostadas[i].classList.remove("size2");
+//     }
+//   }
+// }
 
-window.addEventListener("load", deleteClass);
-window.addEventListener("resize", deleteClass);
+// window.addEventListener("load", deleteClass);
+// window.addEventListener("resize", deleteClass);
